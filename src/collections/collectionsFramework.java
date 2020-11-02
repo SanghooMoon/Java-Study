a05ded40c974fef5b866fb4544e6dc678c5107d3
SanghooMoon <sanghoo.moon@gmail.com>
2020-11-02 19:29:23 +0900

    ch11] 컬렉션 프레임웍

---
+++ src/collections/collectionsFramework.java
@@ -0,0 +1,13 @@
+package collections;
+
+public class collectionsFramework {
+    /**
+     * List 인터페이스 : 순서가 있는 데이터의 잡합. 데이터의 중복을 허용, ArrayList, LinkedList, Stack, Vector 등, ex) 대기자 명단
+     * Set 인터페이스 : 순서를 유지하지 않는 데이터의 집합, 데이터의 중복을 허용하지 않음, HashSet, TreeSet 등, ex) 양의 정수 집합, 소수의 집합
+     * Map 인터페이스 : 키와 값의 쌍으로 이루어진 데이터의 집합, 순서는 유지되지 않고, 키는 중복을 허용하지 않고, 값은 중복을 허용한다. HashMap, TreeMap, Hashtable 등 ex) 우편번호, 지역번호
+     *
+     * Vector, Stack, Hashtable, Properties와 같은 클래스들은 컬렉션 프레임웍이 만들어지기 이전부터 전조해던 것이라 컬렉션 프레임웍의 명명법을 따르진 ㅇ낳음.
+     * Vector, Hashtable은 호환을 위해, 설계를 변경해서 남겨두었지만 가능하면 사용하지 않는것이 좋다.
+     * 그 대신 새로 추가된 ArrayList와 HashMap을 이용하자.
+     */
+}
